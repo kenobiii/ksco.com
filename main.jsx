bea61ec1687fe0b8327f2e42a0962ad78d666c40
kenobiii <79169073+kenobiii@users.noreply.github.com>
2026-04-10 13:13:03 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import KenSilvaV4 from './KenSilvaCo_v4.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <KenSilvaV4 />
-  </StrictMode>
-)
